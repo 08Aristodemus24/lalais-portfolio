00e184a6b40b478a6d562e04610c46cc72e779d5
08Aristodemus24 <MichaelAveuc571@gmail.com>
2025-05-31 19:54:17 +0800

    update

--- client-side/src/routes/+page.svelte
+++ client-side/src/routes/+page.svelte
@@ -7,5 +7,11 @@
 </Hero>
 <Hero section_name="featured">
     <h1 class="featured-header">featured</h1>
-    <!--  -->
+    <div class="featured-container">
+        <div class="featured-images col-one">
+            
+        </div>
+        <div class="featured-images col-two"></div>
+        <div class="featured-images col-three"></div>
+    </div>
 </Hero>
